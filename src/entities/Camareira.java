3f6d3b12a1ed1359ed614d33887ea263579c4817
Daniel Luiz <danielluizdossantos12@gmail.com>
2024-05-02 15:45:53 -0300

    camareira atualizad

--- src/entities/Camareira.java
+++ src/entities/Camareira.java
@@ -1,24 +1,34 @@
 package entities;
 
+import java.util.List;
+
 public class Camareira extends Thread{
 	private String nome;
-	private Quarto quartoALavar;
+	private List<Quarto> quartoALavar;
 
-	public Camareira(String nome) {
+	public Camareira(String nome,List<Quarto> quartos) {
 		super(nome);
-		
+		quartoALavar = quartos;
 	}
-	public void setQuartoALavar(Quarto quartoALavar) {
+	public void setQuartoALavar(List<Quarto> quartoALavar) {
 		this.quartoALavar = quartoALavar;
 	}
 
 	public void arrumarQuarto() {
-		this.quartoALavar.usarOQuarto();
+		this.quartoALavar.get(0).usarOQuarto();
 	}
 
 	public void run() {
-
+		while(quartoALavar ==null) {
+			try {
+				wait();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 		this.arrumarQuarto();
+		this.quartoALavar.remove(0);
 	}
 	public String getNome() {
 		return nome;
--- src/main/Main.java
+++ src/main/Main.java
@@ -12,22 +12,21 @@ public class Main {
 
 	public static void main(String[] args) {
 		List<Quarto> quartos = new ArrayList<>();
+		List<Quarto> quartoALavar = new ArrayList<>();
 		List<Hospede> hospedes = new ArrayList<>();
 		List<Camareira> camareiras = new ArrayList<>();
-		List<Recepcionista> recepcionistas = new ArrayList<>();
+		List<Recepcionista>recepcionistas = new ArrayList<>();
+		
 	
 		while (true) {
 			for(int i=0;i<1;i++) {
 				quartos.add(new Quarto(i+1));
 				hospedes.add(new Hospede("Hospede"+(i+1))); 
-				camareiras.add(new Camareira("Camareira"+(i+1)));
-				recepcionistas.add(new Recepcionista("Recepcionista"+(i+1), hospedes, quartos));
+				camareiras.add(new Camareira("Camareira"+(i+1),quartoALavar));
 			}
-			for(int i=0;i<1;i++) {
-				hospedes.get(i).start();
-				camareiras.get(i).start();
-			}
-		
+			
+			hospedes.get(0).start();
+			camareiras.get(0).start();
 		}
 	}
 }
