435381dc6a0317d437c45d32d11fb42e128b78fa
Vilehauk <151260459+Vilehauk@users.noreply.github.com>
2024-05-06 16:06:34 +0000

    feat: sistema de espera do cliente e insultos

--- src/entities/Hospede.java
+++ src/entities/Hospede.java
@@ -7,6 +7,7 @@ public class Hospede extends Thread {
 	private String nome;
 	private Quarto quartoOndeHospedado;
 	private Integer grupo;
+	private Integer tolerancia;
     private FilaDeQuartosALavar quartosALavar;
 
 	public FilaDeQuartosALavar getQuartosALavar() {
@@ -54,38 +55,71 @@ public class Hospede extends Thread {
 		return grupo;
 	}
 
-    public Quarto getQuarto(){
-        return this.quartoOndeHospedado;
-    }
+	public void darUmaVolta() {
+		System.out.println(this.nome+" foi dar uma volta!");
+		Thread.sleep(1000);
+	}
+
+	public void grieve() {
+		List<String> insults = new ArrayList<String>();
+		insults.add(" nem queria um lugar mesmo.");
+		insults.add(" diz que a concorrência é melhor.");
+		insults.add(" entende o problema, mas não deseja mais um quarto.");
+		insults.add(" disse que vai voltar para a itália.");
+		insults.add(" ameaçou reportar o hotel para o inspetor sanitário local!");
+		insults.add(" tentou subornar um dos recepcionistas, sem sucesso.");
+		insults.add(" era o presidente, que lástima.");
+		insults.add(" era filho do chefe, foi bom trabalhar com vocês.");
+		Random insult = new Random();
+		System.out.println(this.nome+insults.get(insult.nextInt(insults.size())));
+	}
+
+  public Quarto getQuarto(){
+    return this.quartoOndeHospedado;
+  }
 
 	public void run() {
-		Random r = new Random();
-		int numeroDeVezesQueVaiEntrarESair = r.nextInt(10);
-        int i = 0;
-        while(i<numeroDeVezesQueVaiEntrarESair){
-			while(this.quartosALavar.contains(quartoOndeHospedado)) {
-				try {
-					System.out.println(getName() + " esperando a chave estar na recepção...");
-					sleep(1000);
+		if(QuartoOndeHospedado==null) {
+			if(!Recepcionista.roomless.contains(this)) {
+				boolean inQueue = Recepcionista.lodgeRequest(this);
+				if(!inQueue) {
+					this.tolerancia--;
+					if(tolerancia>0) {
+						darUmaVolta();
+					} else {
+						grieve();
+					}
+				}
+			}
+		} else {
+			Random r = new Random();
+			int numeroDeVezesQueVaiEntrarESair = r.nextInt(10);
+	        int i = 0;
+	        while(i<numeroDeVezesQueVaiEntrarESair){
+				while(this.quartosALavar.contains(quartoOndeHospedado)) {
+					try {
+						System.out.println(getName() + " esperando a chave estar na recepção...");
+						sleep(1000);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+	            this.quartoOndeHospedado.entrarNoQuarto(getName());
+	            if(!this.quartosALavar.contains(this.getQuarto())){
+	                this.quartosALavar.push(this.getQuarto());
+	            }            
+	            try {
+					sleep(3000);
 				} catch (InterruptedException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
-				}
-			}
-            this.quartoOndeHospedado.entrarNoQuarto(getName());
-            if(!this.quartosALavar.contains(this.getQuarto())){
-                this.quartosALavar.push(this.getQuarto());
-            }            
-            try {
-				sleep(3000);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} 
-            finally{
-                i++;
-            }      
-        }
+				} 
+	            finally{
+	                i++;
+	            }      
+	        }
+		}
 	}
 	public String getNome() {
 		return nome;
